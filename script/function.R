cff47c2825a60062b6450b180e5c7fad571a8b7f
Takuto Kotsubo <>
2018-04-23 16:17:24 +0900

    CircularRegSim : 乱数列生成 GeneratingEps : 正規乱数の共分散行列パラメータ生成

--- script/function.R
+++ script/function.R
@@ -1,13 +1,27 @@
-CircularRegSim <- function(theta_vec, lag, alpha, Beta){
+# シミュレーションデータを生成する関数
+CircularRegSim <- function(theta_start_vec, len, alpha, Beta, Eps){
   #theta_vec:Angle data, alpha:(2,1), Beta:(2,2,lag)
-  len <- length(theta_vec) 
-  d <- matrix(nrow = 2,ncol = len - lag) 
+  lag <- length(theta_start_vec)
+  d <- matrix(nrow = 2, ncol = len) 
+  mu <- matrix(c(0,0),nrow = 2)
   
-  for(i in 1:len-lag){
+  # 初期値を行列dに格納する.
+  for(i in 1:lag){
+    d[1,i] = cos(theta_start_vec[i])
+    d[2,i] = sin(theta_start_vec[i])
+  }
+  for (i in seq(lag+1,len,1)){
     tmp <- matrix(c(0,0),nrow = 2)
     for(j in 1:lag){
-      tmp = tmp + beta[,,j] %*% matrix(c(cos(theta_vec[i-j]),sin(theta_vec[i-j])),nrow = 2)
+      tmp = tmp + (beta[,,j] %*% d[,i-lag])
     }
-    d[,i] = alpha + tmp 
+    d[,i] = alpha + tmp + mvrnorm(1,mu,Eps)
   }
+  return(d)
+}
+
+# 正規乱数のパラメータより, 分散共分散行列を計算する関数
+GeneratingEps <- function(sigma_c, sigma_s, rho){
+  matrix(c(sigma_c^2,rho*sigma_c*sigma_s,rho*sigma_c*sigma_s,sigma_s^2),nrow=2) %>% 
+    return()
 }
