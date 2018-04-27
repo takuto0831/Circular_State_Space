0bae8eb06fecf398f82a64698b5011dbd37c64c2
takuto0831 <aseries2765@gmail.com>
2018-04-27 15:35:07 +0900

    プロット追加

--- script/function.R
+++ script/function.R
@@ -22,6 +22,33 @@ CircularRegSim <- function(theta_start_vec, len, alpha, beta, Eps){
   return(d)
 }
 
+# シミュレーションデータを生成する関数
+CircularRegPred <- function(vec, lag, alpha, beta, Eps){
+  #theta_vec:Angle data, alpha:(2,1), beta:(2,2,lag)
+  len <- length(vec)
+  mu <- matrix(c(0,0),nrow = 2)
+  d <- matrix(nrow = 3, ncol = len) 
+  
+  # 初期値を行列dに格納する.
+  for(i in 1:lag){
+    d[1,i] = i # 次数
+    d[2,i] = cos(vec[i])
+    d[3,i] = sin(vec[i])
+  }
+  for (i in seq(lag+1,len,1)){
+    tmp <- matrix(c(0,0),nrow = 2)
+    for(j in 1:lag){
+      tmp = tmp + (beta[[j]] %*% d[2:3,i-lag])
+    }
+    d[1,i] = i
+    d[2:3,i] = alpha + tmp + as.matrix(mvrnorm(1,mu,Eps))
+  }
+  return(d %>% 
+           t() %>% 
+           as.data.frame() %>%
+           rename(p=V1, cos=V2, sin=V3))
+}
+
 # 正規乱数のパラメータより, 分散共分散行列を計算する関数
 GeneratingEps <- function(sigma_c, sigma_s, rho){
   matrix(c(sigma_c^2,rho*sigma_c*sigma_s,rho*sigma_c*sigma_s,sigma_s^2),nrow=2) %>% 
