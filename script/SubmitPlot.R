d57a5444329c3decca0cc0c519c471b2b8580645
takuto0831 <aseries2765@gmail.com>
2018-06-13 21:55:41 +0900

    更新

--- script/SubmitPlot.R
+++ script/SubmitPlot.R
@@ -5,7 +5,7 @@ library(ggthemes)
 library(circular)
 
 # project normal func
-ff <- function(theta,mu,Sigma){
+projected_normal_circular <- function(theta,mu,Sigma){
   u = matrix(c(cos(theta),sin(theta)),ncol=1)
   A = t(u) %*% solve(Sigma) %*% u
   B = t(u) %*% solve(Sigma) %*% mu
@@ -108,3 +108,23 @@ wind_data %>%
   geom_histogram(binwidth = 0.2, colour = "black", fill = "grey") + 
   labs(x="Wind directions in dadians") +
   theme_bw()
+
+################ p(theta;Sigma) = p(theta;c*Sigma)の確認 #################
+###### 一致しない
+mu = matrix(c(-0.19,2.09),ncol=1); 
+sigma1 = 1.58; rho = -0.84; sigma2 = sqrt( 1 / (sigma1^2*(1-rho^2)) );
+Sigma = matrix(c(sigma1^2,rho*sigma1*sigma2,
+                 rho*sigma1*sigma2,sigma2^2),byrow=T,ncol=2)
+
+ff <- function(x) dpnorm(x, mu=mu, sigma=Sigma*2)
+ff1 <- function(x) dpnorm(x, mu=mu, sigma=Sigma)
+par(mar = c(0,0,0,0))
+curve.circular(ff, shrink=1.6,n=100000,cex=1.4,lwd=2)
+curve.circular(ff1, shrink=1.6,n=10000,cex=1.4,lwd=2)
+
+# 予測値をプロット
+theta <- seq(0, 2 * pi ,0.1)
+x <- foreach(i=circular(theta), .combine = c) %do% projected_normal_circular(i,mu,Sigma*2) 
+data.frame(theta = theta,prob = x) %>% 
+  ggplot(aes(x = theta, y = prob)) + 
+  geom_line() 
